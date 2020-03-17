72c0f57afa44a282453eb3d523ff7aebf0979289
phette23 <phette23@gmail.com>
2020-03-17 16:43:04 -0700

    more complete name change script
    accept CSV of name change data where we do not know the patron's Koha ID yet
    then find the Koha IDs via GET /api/v1/patrons?userid=X route (NOTE: this
    route seems to have a bug and sometimes throws HTTP 500 when it should return
    empty array) then execute the name change
    works on CLI

--- name_change.py
+++ name_change.py
@@ -1,42 +1,59 @@
-""" change a patron's name via the REST API """
-import json
-import urllib3
+"""
+change a patron's name via the REST API
+usage:
+    python name_change.py changes.csv
+"""
+import config
+from koha_patron.oauth import get_token
+from koha_patron.request_wrapper import request_wrapper
 
-import requests
-
-
-with open('config.json', 'r') as f:
-    config = json.load(f)
-
-# ByWater's SSL cert causes problems so every request has verify=False
-# and we do this to silence printed warnings
-urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
-
-read_only_patron_fields = ("anonymized", "restricted", "updated_on")
+# set a global OAUTH token that all requests can use
+token = get_token()
 
 
 def remove_readonly_fields(patron):
+    """ remove readonly fields from patron dict
+    must run patron records through this fn before PUTting to Koha """
+    read_only_patron_fields = ("anonymized", "restricted", "updated_on")
     for field in read_only_patron_fields:
         patron.pop(field, None)
     return patron
 
 
-def get_token():
-    """ Acquire an OAuth token for Koha
+def get_patron(username):
+    """ Get a complete patron record given a username
 
-    returns: OAuth token (string) """
-    data = {
-        "client_id": config["client_id"],
-        "client_secret": config["client_secret"],
-        "grant_type": "client_credentials",
-    }
-    response = requests.post(config['api_root'] + '/oauth/token',
-                    data=data, verify=False)
-    token = str(response.json()['access_token'])
-    return token
+    arguments:
+        username (str): patron's Koha (same as CCA) username
+    returns:
+        patron (dict): patron dict from Koha API
+    throws:
+        HTTP exceptions from Requests
+    """
+    s = request_wrapper()
+    response = s.get('{}/patrons?userid={}'.format(config.api_root, username))
+    # we cannot rfs because sometimes API returns 500 error when it should send an empty array
+    # response.raise_for_status()
+    data = response.json()
+    if len(data["errors"]):
+        print('Errors in Koha API response. It is likely that there is no patron with a "{}" username.'.format(username))
+    if len(data) == 1:
+        return data[0]
+    elif len(data) > 1:
+        # multiple patrons, look for correct one
+        for patron in data:
+            if patron["userid"] == username:
+                return patron
+        # no one matched exactly
+        print('Multiple patron records were returned for `userid = "{}"` query but none were an exact match.'.format(username))
+        return None
+    else:
+        # => len(response.json()) == 0
+        print('No patron with username "{}".'.format(username))
+        return None
 
 
-def name_change(patron_id, name, token=None):
+def name_change(patron_id, name):
     """ Change a Patron's name via the Koha API
 
     arguments:
@@ -46,23 +63,13 @@ def name_change(patron_id, name, token=None):
             "surname": "new surname" }
             You only need to provide changed name fields, not both.
         token (str): OAuth access token
-
     returns:
         HTTP response object from requests
-
     throws:
         HTTP exceptions from Requests
     """
-    if token is None:
-        token = get_token()
-
-    headers = {
-        'Accept': 'application/json',
-        'Authorization': 'Bearer ' + token,
-        'Content-Type': 'application/json',
-    }
-    r = requests.get(config['api_root'] + '/patrons/{}'.format(str(patron_id)),
-        headers=headers, verify=False)
+    s = request_wrapper()
+    r = s.get(config.api_root + '/patrons/{}'.format(str(patron_id)))
     r.raise_for_status()
     patron = remove_readonly_fields(r.json())
 
@@ -72,19 +79,45 @@ def name_change(patron_id, name, token=None):
     if name.get("surname"):
         patron["surname"] = name["surname"]
 
-    response = requests.put(config['api_root'] + '/patrons/{}'.format(str(patron_id)),
-        json=patron, headers=headers, verify=False)
-    # response.raise_for_status()
+    response = s.put(config.api_root + '/patrons/{}'.format(str(patron_id)),
+        json=patron)
+    response.raise_for_status()
     return response
 
 
+# CSV I've seen so far has rows named User ID,First Name - Proposed,Last Name - Proposed
+def handle_csv_row(row):
+    # row is a dict, "User ID" is formatted "username / Givenname Surname"
+    patron = get_patron(row["User ID"].split(' / ')[0])
+    if patron is not None:
+        name_change(patron["patron_id"], {
+            "firstname": row["First Name - Proposed"],
+            "surname": row["Last Name - Proposed"]
+        })
+        print('Changed user #{} name "{} {}" => "{} {}"'.format(
+            patron["patron_id"], patron["firstname"], patron["surname"],
+            row["First Name - Proposed"], row["Last Name - Proposed"]
+        ))
+
+
 if __name__ == '__main__':
-    # "TEST Undergrad" patron
-    # library-staff.cca.edu/cgi-bin/koha/members/moremember.pl?borrowernumber=14831
-    token = get_token()
-    r = name_change(14831, {"firstname": "TESTnewname", "surname": "Smith"}, token)
-    print('Name changed, Koha API response:')
-    print(r, r.status_code, r.text)
-    print('Changing name back now...')
-    # change it back
-    name_change(14831, {"firstname": "TEST", "surname": "Undergrad"}, token)
+    import csv, sys
+    if '.csv' in sys.argv[2]:
+        filename = sys.argv[2]
+        print('Processing rows in {} file...'.format(filename))
+        with open(filename, 'r') as fh:
+            for row in csv.DictReader(fh):
+                handle_csv_row(row)
+    else:
+        # test change on "TEST Undergrad" patron
+        # library-staff.cca.edu/cgi-bin/koha/members/moremember.pl?borrowernumber=14831
+        r = name_change(14831, {"firstname": "TESTnewname", "surname": "Smith"})
+        print('Name changed, Koha API response:')
+        print(r, r.status_code, r.text)
+        print('\nKoha patron record:')
+        print(get_patron('stest1'))
+        print('Changing name back now...')
+        # change it back
+        name_change(14831, {"firstname": "TEST", "surname": "Undergrad"})
+        print('\nKoha patron record:')
+        print(get_patron('stest1'))
