0ccf770cf4bdff3ff4931959400ec0f8929159e2
Eric Phetteplace <phette23@gmail.com>
2021-05-12 12:09:04 -0700

    some python linting, rename file

--- create_koha_csv.py
+++ create_koha_csv.py
@@ -26,9 +26,9 @@ parser.add_argument('-e', '--employee-expiry', type=str, default='2020-05-31',
 args = parser.parse_args()
 
 today = datetime.date.today()
-emp_file = 'employee_data.json'
-stu_file = 'student_data.json'
-out_file = str(today.isoformat()) + '-koha-patrons.csv'
+EMP_FILE = 'employee_data.json'
+STU_FILE = 'student_data.json'
+OUT_FILE = str(today.isoformat()) + '-koha-patrons.csv'
 koha_fields = [ 'branchcode', 'cardnumber', 'categorycode', 'dateenrolled',
 'dateexpiry', 'email', 'firstname', 'patron_attributes', 'surname', 'userid', ]
 
@@ -49,7 +49,8 @@ def make_student_row(student):
         "dateexpiry": args.student_expiry,
         "email": student["inst_email"],
         "firstname": student["first_name"],
-        "patron_attributes": "UNIVID:{},STUID:{}".format(student["universal_id"], student["student_id"]),
+        "patron_attributes": "UNIVID:{},STUID:{}".format(
+            student["universal_id"], student["student_id"]),
         "surname": student["last_name"],
         "userid": student["username"],
     }
@@ -92,6 +93,9 @@ def make_employee_row(person):
     elif person.get("department"):
         person["prodep"] = person["department"]
 
+    # skip inactive special programs faculty
+    if person["job_profile"] == "Special Programs Instructor (inactive)":
+        return None
     # we assume etype=Instructors => special programs faculty
     if (person["etype"] == "Instructors"
         and person["job_profile"] != "Special Programs Instructor"):
@@ -121,29 +125,34 @@ def make_employee_row(person):
         print("""No mapping in koha_mappings.fac_depts for faculty/staff prodep
         "{}", see patron {}""".format(person["prodep"], person["username"]))
 
+    if person["prodep"] is None:
+        print(person)
+
     return patron
 
-if os.path.exists(stu_file):
+if os.path.exists(STU_FILE):
     print('Adding students to Koha patron CSV.')
-    with open(stu_file, 'r') as file:
+    with open(STU_FILE, 'r') as file:
         students = json.load(file)["Report_Entry"]
-        with open(out_file, 'w') as output:
+        with open(OUT_FILE, 'w') as output:
             writer = csv.DictWriter(output, fieldnames=koha_fields)
             writer.writeheader()
-            for student in students:
-                row = make_student_row(student)
-                if row: writer.writerow(row)
+            for stu in students:
+                row = make_student_row(stu)
+                if row:
+                    writer.writerow(row)
 
-if os.path.exists(emp_file):
+if os.path.exists(EMP_FILE):
     print('Adding Faculty/Staff to Koha patron CSV.')
-    with open(emp_file, 'r') as file:
+    with open(EMP_FILE, 'r') as file:
         employees = json.load(file)["Report_Entry"]
         # open in append mode & don't add header row
-        with open(out_file, 'a') as output:
+        with open(OUT_FILE, 'a') as output:
             writer = csv.DictWriter(output, fieldnames=koha_fields)
             for employee in employees:
                 row = make_employee_row(employee)
-                if row: writer.writerow(row)
+                if row:
+                    writer.writerow(row)
 
 print('Done! Upload the CSV at \
 https://library-staff.cca.edu/cgi-bin/koha/tools/import_borrowers.pl')
@@ -157,7 +166,7 @@ if path.strip() != '':
             print('Error: unable to create directory at path "{}"'.format(path))
             exit(1)
 
-    for name in [stu_file, emp_file, out_file]:
+    for name in [STU_FILE, EMP_FILE, OUT_FILE]:
         os.renames(name, os.path.join(path, name))
 else:
     print('Files were not archived.')
