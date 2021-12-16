72e042be1571cb6cf080b04759973008c6249634
Eric Phetteplace <phette23@gmail.com>
2021-12-16 13:09:26 -0800

    add counting, summary at end of script

--- patch_prox_num.py
+++ patch_prox_num.py
@@ -44,6 +44,7 @@ def update_patron(koha, workday, prox):
     try:
         response.raise_for_status()
     except HTTPError:
+        totals["error"] += 1
         print('Error', response)
         print('HTTP Response Headers', response.headers)
         print(response.text)
@@ -53,25 +54,33 @@ def update_patron(koha, workday, prox):
             workday['last_name'],
             prox
         )
+        return False
+
+    totals["updated"] += 1
+    return True
 
 
 def missing_patron(workday):
+    totals["missing"] += 1
     print('Could not find any patrons with a userid of "{}"'.format(workday['username']))
     missing.append(workday)
 
 
 def main():
     prox_map = create_prox_map('data/prox.csv')
-    with open('data/employee_data.json', 'r') as file:
+    with open('data/student_data.json', 'r') as file:
         people = json.load(file)["Report_Entry"]
 
+    # global vars that other functions need to access
     global missing
     missing = []
-    # uncomment the next line to test on just Libraries staff
-    # people = [p for p in people if p.get('department', None) == 'Libraries']
+    global totals
+    totals = { "missing": 0, "error": 0, "updated": 0, "no_prox": 0}
     global http
     http = request_wrapper()
-    # clarify where data is coming from/going to with dict names
+
+    # uncomment the next line to test on just Libraries staff
+    # people = [p for p in people if p.get('department', None) == 'Libraries']
     for workday in people:
         if workday['universal_id'] in prox_map:
             prox = prox_map[workday['universal_id']]
@@ -82,6 +91,7 @@ def main():
             try:
                 response.raise_for_status()
             except HTTPError:
+                totals["error"] += 1
                 print('Error', response)
                 print('HTTP Response Headers', response.headers)
                 print(response.text)
@@ -110,11 +120,13 @@ def main():
                     elif len(patrons) == 1:
                         update_patron(patrons[0], workday, prox)
                     else:
+                        # this probably isn't possible...
                         raise Exception(('Found multiple patrons with the username "{}", '
                                         'something has gone horribly wrong. Patron records: {}')
                                         .format(workday['username'], patrons))
 
         else:
+            totals["no_prox"] += 1
             print('{} {} ({}) has universal ID {} and no prox number'.format(
                 workday['first_name'],
                 workday['last_name'],
@@ -129,3 +141,9 @@ def main():
 
 if __name__ == '__main__':
     main()
+    print("Summary:\n\t- {}\n\t- {}\n\t- {}\n\t- {}".format(
+        "Updated Patrons: " + str(totals["updated"]),
+        "No Prox number: " + str(totals["no_prox"]),
+        "Missing from Koha: " + str(totals["missing"]),
+        "Errors: " + str(totals["error"]),
+    ))
