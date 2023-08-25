267c464873925585cdb1127144d1ce8954e8c6b7
phette23 <phette23@gmail.com>
2023-08-25 09:10:41 -0700

    fix trim_two_lines, use re.search instead of re.match

--- koha_patron/utils.py
+++ koha_patron/utils.py
@@ -4,8 +4,8 @@ def trim_first_two_lines(file_name, string):
     with open(file_name, "r") as f:
         lines = f.readlines()
         # Check if the first line matches the string
-        if re.match(string, lines[0]):
-        # Remove the first two lines
+        if re.search(string, lines[0]):
+            # Remove the first two lines
             lines = lines[2:]
 
     with open(file_name, "w") as f:
