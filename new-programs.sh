761df5a70346cee79520e889640e810b6adcde1a
phette23 <phette23@gmail.com>
2024-06-04 10:20:10 -0700

    fix: shellcheck on new-programs.sh

--- new-programs.sh
+++ new-programs.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-jq '.Report_Entry[].primary_program' student_data.json | sort | uniq > data/$(date "+%Y-%m-%d")-student-majors.txt
-jq '.Report_Entry[].program' employee_data.json | sort | uniq | sed '/null/d' > data/$(date "+%Y-%m-%d")-staff-depts.txt
-jq '.Report_Entry[].department' employee_data.json | sort | uniq | sed '/null/d' >> data/$(date "+%Y-%m-%d")-staff-depts.txt
+jq '.Report_Entry[].primary_program' student_data.json | sort | uniq > "data/$(date "+%Y-%m-%d")-student-majors.txt"
+jq '.Report_Entry[].program' employee_data.json | sort | uniq | sed '/null/d' > "data/$(date "+%Y-%m-%d")-staff-depts.txt"
+jq '.Report_Entry[].department' employee_data.json | sort | uniq | sed '/null/d' >> "data/$(date "+%Y-%m-%d")-staff-depts.txt"
 
 echo -e "\nSTAFF DEPTS DIFF:\n"
 git diff --no-index --color-words data/*-staff-depts.txt
